c60d7564d444c40cc6bcdf9ae49178cab1e975cb
Raiffil <natalkap2@gmail.com>
2025-09-18 12:13:37 +0200

    [add] Press space to start

--- Pidgeon/Pidgeon.py
+++ Pidgeon/Pidgeon.py
@@ -66,9 +66,9 @@ menu_btn    = pygame.Rect(290, 565, 420, 140)
 
 def reset_game():
     global x, y, velocity, objects, score, last_obj_spawn_time, frame_index
-    global last_update, animate
-    # Starting position
-    y = 200
+    global last_update, animate, game_ready
+
+    y = 200 #Starting position
     x = 500
     velocity = 0 #How fast the bird is moving at the time
     objects = []  #Objects list
@@ -77,6 +77,7 @@ def reset_game():
     frame_index = 0
     last_update = pygame.time.get_ticks()
     animate = True
+    game_ready = True #True when waiting for first jump
 
 reset_game()
 
@@ -102,6 +103,7 @@ while running:
             running = False         #End Loop
 
         if state == 'menu':
+            game_ready = False
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 mx, my = event.pos
                 if start_btn.collidepoint(mx, my):
@@ -114,10 +116,15 @@ while running:
 
         elif state == 'game':
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    state = "menu" #Go back to menu on ESC
-                if event.key == pygame.K_SPACE:
-                    velocity += jump  #If Space is pressed → move up
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        state = "menu" #Go back to menu on ESC
+                    if event.key == pygame.K_SPACE:
+                        if game_ready:
+                            game_ready = False
+                            velocity += jump  #If Space is pressed → move up
+                        else:
+                            velocity += jump
 
         elif state == "restart":
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
@@ -136,52 +143,53 @@ while running:
         #pygame.draw.rect(screen, (0,0,255), quit_btn, 2)
 
     elif state == "game":
-        velocity += gravity  #Gravity works as long as game is running
-        clipped_velocity = np.clip(velocity, -100, 1)  #Clip velocity so the gravity is more controlled
-        y += clipped_velocity  #Speed of bird is stored in velocity to make jump smoother
+        if not game_ready:
+            velocity += gravity  #Gravity works as long as game is running
+            clipped_velocity = np.clip(velocity, -100, 1)  #Clip velocity so the gravity is more controlled
+            y += clipped_velocity  #Speed of bird is stored in velocity to make jump smoother
 
-        if y < 0: #Prevent falling off the screen
-            y = 0
-        elif y > WinHeight - BirdHeight:
-            y = WinHeight - BirdHeight
+            if y < 0: #Prevent falling off the screen
+                y = 0
+            elif y > WinHeight - BirdHeight:
+                y = WinHeight - BirdHeight
 
-        if now - last_obj_spawn_time > obj_spawn_interval:
-            last_obj_spawn_time = now  #Spawns objects
-            spawn_x = WinWidth + 20
-            objects.append(create_obj(spawn_x))
+            if now - last_obj_spawn_time > obj_spawn_interval:
+                last_obj_spawn_time = now  #Spawns objects
+                spawn_x = WinWidth + 20
+                objects.append(create_obj(spawn_x))
 
-        bird_rect = pygame.Rect(x, int(y), BirdWidth, BirdHeight) #Bird hitbox
-        new_obj = []
-        for obj in objects:
-            obj['x'] -= obj_speed  #Make objects move left
+            bird_rect = pygame.Rect(x, int(y), BirdWidth, BirdHeight) #Bird hitbox
+            new_obj = []
+            for obj in objects:
+                obj['x'] -= obj_speed  #Make objects move left
 
-            top_obj = pygame.Rect(int(obj['x']), 0, obj_width, int(obj['gap_y']))
-            bottom_obj = pygame.Rect(int(obj['x']), int(obj['gap_y'] + obj['gap_size']), obj_width, WinHeight - int(obj['gap_y'] + obj['gap_size']))
+                top_obj = pygame.Rect(int(obj['x']), 0, obj_width, int(obj['gap_y']))
+                bottom_obj = pygame.Rect(int(obj['x']), int(obj['gap_y'] + obj['gap_size']), obj_width, WinHeight - int(obj['gap_y'] + obj['gap_size']))
 
-            #Collision check per pipe
-            if bird_rect.colliderect(top_obj) or bird_rect.colliderect(bottom_obj):
-                print("Collision! Game over. Score:", score)
-                if score > high_score:
-                    high_score = score
-                    save_high_score(high_score)  #Write new high score to file
-                state = "restart"
-                break
+                #Collision check per pipe
+                if bird_rect.colliderect(top_obj) or bird_rect.colliderect(bottom_obj):
+                    print("Collision! Game over. Score:", score)
+                    if score > high_score:
+                        high_score = score
+                        save_high_score(high_score)  #Write new high score to file
+                    state = "restart"
+                    break
 
-            #When object passes the bird's x coordinate get a point
-            if not obj['passed'] and (obj['x'] + obj_width) < x:
-                obj['passed'] = True
-                score += 1
+                #When object passes the bird's x coordinate get a point
+                if not obj['passed'] and (obj['x'] + obj_width) < x:
+                    obj['passed'] = True
+                    score += 1
 
-            #Keep the object if it's still on screen (or a bit past left edge)
-            if obj['x'] + obj_width > -50:
-                new_obj.append(obj)
+                #Keep the object if it's still on screen (or a bit past left edge)
+                if obj['x'] + obj_width > -50:
+                    new_obj.append(obj)
 
-            #Save rects inside the object so they can be drawn after the background
-            obj['top_rect'] = top_obj
-            obj['bottom_rect'] = bottom_obj
+                #Save rects inside the object so they can be drawn after the background
+                obj['top_rect'] = top_obj
+                obj['bottom_rect'] = bottom_obj
 
-        #Replace objects list with surviving objects
-        objects = new_obj
+            #Replace objects list with surviving objects
+            objects = new_obj
 
         #Animate bird
         now = pygame.time.get_ticks()
@@ -220,5 +228,10 @@ while running:
         high_text  = font2.render(f" {high_score}", True, (71, 37, 114))
         screen.blit(score_text, (960, 410))
         screen.blit(high_text, (530, 410))
+
+    #Press space to start
+    if game_ready:
+        ready_text = font2.render("Press SPACE to start", True, (255, 255, 255))
+        screen.blit(ready_text, (WinWidth // 2 - ready_text.get_width() // 2, 400))
 
     pygame.display.flip()         #Update the window
